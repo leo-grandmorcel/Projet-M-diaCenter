dc105a9f06d8fc9a6b39d7570f95299b1b2f586a
LGRAND-MORCEL <leo.grandmorcel@ynov.com>
2022-02-15 09:02:05 +0100

    push

--- test.sh
+++
@@ -1,3 +0,0 @@
-scp /home/pol/Documents/work/projet/projet_infra/Projet-M-diaCenter/preset.sh  debian@92.222.36.79:/home/debian/
-scp /home/pol/Documents/work/projet/projet_infra/Projet-M-diaCenter/nginx.conf.skeleton  debian@92.222.36.79:/home/debian/
-scp /home/pol/Documents/work/projet/projet_infra/Projet-M-diaCenter/Setup.sh debian@92.222.36.79:/home/debian
